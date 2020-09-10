d9291664c7db250f23dbec823c3e9d750fcfba05
Jisan-Mia <jisanmia47@gmail.com>
2020-09-10 23:55:32 +0600

    styl.css and script.js linked to html file

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// let hexagoni = document.getElementsByClassName("hexagon");
 
-// for (let i = 0; i < hexagoni.length; i++) {
-//   hexagoni[i].addEventListener("mouseenter", () => {
-//     hexagoni[i].classList.add("light");
-//     setTimeout(function () {
-//       hexagoni[i].classList.remove("light");
-//     }, 800);
-//   });
-// }
 let hexagon = document.getElementsByClassName("hexagon");
 
 for (let i = 0; i < hexagon.length; i++){
